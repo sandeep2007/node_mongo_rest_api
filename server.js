c5ac4f617c25b2a14df2441e07e957bd6509b9ed
Sandeep Kumar <ki.sandeep11@gmail.com>
2023-11-18 19:01:09 +0530

    Docker ecosystem implemented

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-require('dotenv/config')
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require("dotenv/config");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-const postRoutes = require('./routes/posts')
-app.use('/posts', postRoutes)
-
-
+const postRoutes = require("./routes/posts");
+app.use("/posts", postRoutes);
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.send('NodeJS')
-})
+app.get("/", (req, res) => {
+  res.send("NodeJS");
+});
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-    console.log('DB Connected')
-})
+  console.log("DB Connected");
+});
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server running on PORT:${PORT}`);
+});
